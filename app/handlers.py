b43d58c71ba831c886be259a719bfa3c02103bc0
saint-sim <nikita.solovey.061606@gmail.com>
2025-06-05 23:12:01 +0300

    Add user's input - Add fsm for parsing user's message

--- app/handlers.py
+++ app/handlers.py
@@ -2,21 +2,35 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
 
 router = Router()
+
+class Find_weather(StatesGroup):
+    place = State()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer('привет', reply_markup=kb.to_menu)
 
 @router.message(F.text.lower() == 'меню')
-async def cmd_start(message: Message):
+async def cmd_menu(message: Message, state: FSMContext):
     await message.answer('Это меню, выберите что хотите)', reply_markup=kb.menu)
+    await state.clear()
 
-@router.message(F.text.lower() == 'ввести место')
-async def cmd_start(message: Message):
-    await message.answer('Выберите место', reply_markup=kb.menu)
+@router.message(F.text.lower() == 'узнать погоду')
+async def find_weather(message: Message, state: FSMContext):
+    await state.set_state(Find_weather.place)
+    await message.answer('Выберите место', reply_markup=kb.to_menu)
 
+@router.message(Find_weather.place)
+async def place(message: Message, state: FSMContext):
+    await state.update_data(place=message.text)
+    data = await state.get_data()
+    await message.answer(f'Ваше место: {data['place']}', reply_markup=kb.to_menu)
+    await state.clear()
+    
 
--- app/keyboards.py
+++ app/keyboards.py
@@ -2,7 +2,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 to_menu = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Меню')]],
-                           resize_keyboard = True)
+                           resize_keyboard = True,)
 
-menu = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Ввести место')]],
-                           resize_keyboard = True)
+menu = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Узнать погоду')]],
+                           resize_keyboard = True,)
+
+choose_weather = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Погода на сегодня'),
+                                                KeyboardButton(text='Погода на вчера'),
+                                                KeyboardButton(text='Погода на неделю')]],
+                                                resize_keyboard = True,)
